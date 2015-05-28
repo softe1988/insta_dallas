9495a848c211596e424bd5a7df0df149dfb94b63
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-05-28 14:49:56 +0000

    added key and pictures display

---
+++ instaclowns/instaclown.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'unirest'
+require 'bundler'
+
+get '/' do  
+  @title = 'Photos'  
+  @photos = recent_photos
+  erb :all_photos 
+end  
+
+get '/:resolution/:hashtag' do
+  @title = 'Photos'  
+  @photos = recent_photos(params[:hashtag],params[:resolution])
+  erb :all_photos
+end
+
+post '/search' do
+  @title = params["hashtag"].capitalize
+  @photos = recent_photos(params["hashtag"],params["resolution"])
+  erb :all_photos
+end
+
+def recent_photos (hashtag = "clown", resolution = "thumbnail")
+  @photos = []
+  instagram_response = "https://api.instagram.com/v1/tags/#{hashtag}/media/recent?client_id=007b83cad0054e0982b91d676e6bca93"
+
+  response = Unirest.get(instagram_response)
+  photos_array = response.body["data"]
+
+  photos_array.each do |photo|
+    image = photo["images"][resolution]["url"]
+    @photos << image
+  end
+
+  @photos
+end 
